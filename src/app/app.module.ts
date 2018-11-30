d2e9682d35bdd428016024adaedcbe4d65a241fa
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2018-11-30 15:24:17 +0100

    Added Token Interceptor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TokenInterceptor } from './services/token-interceptor';
 import { StreamsRoutingModule } from './modules/streams-routing.module';
 import { AuthRoutingModule } from './modules/auth-routing.module';
 import { AuthModule } from './modules/auth.module';
@@ -7,11 +8,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StreamsModule } from './modules/streams.module';
 import { CookieService } from 'ngx-cookie-service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AuthModule, AuthRoutingModule, StreamsModule, StreamsRoutingModule],
-  providers: [CookieService],
+  providers: [
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
